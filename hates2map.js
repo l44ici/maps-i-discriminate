cdcb226211f355a3c218b33c885e155696341694
l44ici <laaici.b.ulep@student.uts.edu.au>
2025-10-07 20:41:09 +1100

    ok go

--- hates2map.js
+++ hates2map.js
@@ -110,7 +110,7 @@
   async function buildMap(){
     if(typeof L==="undefined"){console.error("[B2M] Leaflet missing");return;}
 
-    const map=L.map(ROOT_ID,{zoomControl:true,minZoom:3,maxZoom:12, maxBounds: AU_CLAMP_BOUNDS, maxBoundsViscosity: 1.0, worldCopyJump: false, intertia: false});
+    const map=L.map('b2m-map',{zoomControl:true,minZoom:3,maxZoom:12, maxBounds: AU_CLAMP_BOUNDS, maxBoundsViscosity: 1.0, worldCopyJump: false, intertia: false});
     map.fitBounds(AU_BOUNDS);
 
     const [states,regions]=await Promise.all([fetchJSON(STATES_URL),fetchJSON(REGIONS_URL)]);
